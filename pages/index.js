0b03bd393b9202ed31b4145b0f7cb8ad8c017f63
Camila Zucchi <camilazucchi@outlook.com>
2023-05-29 17:22:11 -0300

    refactor: add next.js link component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Alura Cases - Home</h1>
-            <Link href="/faq">Ir para o FAQ</Link>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Alura Cases - Home</h1>
+      <Link href="/faq" passHref>
+        Ir para o FAQ
+      </Link>
+    </div>
+  );
 }
