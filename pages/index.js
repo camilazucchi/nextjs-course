ab1941c3439a3faa34438aefe087141cac3f7438
Camila Zucchi <camilazucchi@outlook.com>
2023-05-30 16:58:29 -0300

    style: add next.js styling css-in-js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Link from "../src/components/Link";
 
+
+
+function Title({ children }) {
+  return (
+    <>
+      <h1>{children}</h1>
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+      `}</style>
+    </>
+  );
+}
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
-      <Link href="/faq">Ir para a página do FAQ</Link>
+        <Title>Alura Cases - Home</Title>
+        <Link href="/faq">Ir para a página do FAQ</Link>
     </div>
   );
 }
